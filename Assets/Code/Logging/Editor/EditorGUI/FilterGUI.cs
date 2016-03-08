6c386e334914e007c0983e6c3cc092eb92baf3fb
Brian McAleese <brian.mcaleese@eutechnyx.com>
2016-03-08 17:00:58 +0000

    Starting work on gui interfaces

---
+++ Assets/Code/Logging/Editor/EditorGUI/FilterGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FilterGUI : IPaperGUI
+{
+
+	public void DrawGUI( EditorLogStore store )
+	{ 
+		foreach( var channel in System.Enum.GetValues(typeof( LogChannel) ) )
+		{
+			GUILayout.BeginHorizontal();
+
+			if ( GUILayout.Button( channel.ToString() ) )
+				Debug.Log( " got button press" );
+
+			GUILayout.EndHorizontal();
+		}
+	}
+}
